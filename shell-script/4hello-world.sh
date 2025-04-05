3e14b53fc99517668bc358017706f46924937ed8
pavanreddy0406 <pavanreddyyeddala@gmail.com>
2025-04-05 06:35:42 +0000

    chnages add 2

--- shell-script/4hello-world.sh
+++ shell-script/4hello-world.sh
@@ -4,4 +4,4 @@ NUMBER1=$1
 NUMBER2=$2
 #I i need to run this commend inside the shell add 2 numbers
 SUM=$((NUMBER1+NUMBERS2))
-echo "addition of 2 numbers is:$SUM
+echo "addition of 2 numbers is:$SUM"

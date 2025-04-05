02533ca5892e2ace3242b68710cdafe8639b31eb
pavanreddy0406 <pavanreddyyeddala@gmail.com>
2025-04-05 06:30:54 +0000

    adding 2 numbers

---
+++ shell-script/4hello-world.sh
@@ -0,0 +1,7 @@
+#!bin/bash/
+#this is comment we nedd to develops aprogram that can add 2 numbers
+NUMBERS1=$1
+NUMBERS2=$2
+#I i need to run this commend inside the shell add 2 numbers
+SUM=$((NUMBER1+NUMBERS2))
+echo "addition of 2 numbers is:$SUM
